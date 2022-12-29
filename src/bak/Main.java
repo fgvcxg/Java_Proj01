e5b93128fe3bb84210631d33132e484b480715ca
Junho Lee <fgvcxg00@gmail.com>
2022-12-29 17:24:51 +0900

    22 12 29

--- src/bak/Main.java
+++ src/bak/Main.java
@@ -9,44 +9,21 @@ public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		int t = sc.nextInt();
+		
 		int a = sc.nextInt();
-	
+		int b = sc.nextInt();
 		
-		String[] arr = new String[a];
-		
-		sc.nextLine();
-		
-		for(int i = 0; i <a ; i++) {
-			arr[i] = sc.nextLine();
+		for(int i=0;i<t;i++) {
+			
 		}
-		sc.close();
-		
-		Arrays.sort(arr, new Comparator<String>(){
 
-			@Override
-			public int compare(String o1, String o2) {
-				// TODO Auto-generated method stub
-				if(o1.length() == o2.length()) {
-					return o1.compareTo(o2);
-				}
-				else {
-					return o1.length() - o2.length();
-				}
-			}
-		});
 		
-		System.out.println(arr[0]);
-		for (int i = 1; i < a; i++) {
-			// 중복되지 않는 단어만 출력
-			if (!arr[i].equals(arr[i - 1])) {
-				System.out.println(arr[i]);
-			}
-		}
-		
-	
 
 	}
-		
+	
 }
+
+
 
 
